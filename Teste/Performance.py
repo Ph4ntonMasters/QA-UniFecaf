e65283995d79966d77ca094ba00bcbc848175d88
Márcio Augusto de Jesus Silva <109991895+Ph4ntonMasters@users.noreply.github.com>
2025-05-23 08:00:16 -0300

    Create Performance.py

---
+++ Teste/Performance.py
@@ -0,0 +1,16 @@
+Link : https://colab.research.google.com/drive/1aaBehxAd2CftJbyNLJuirWilTApM1NCp?authuser=1#scrollTo=ZX9BeWRIkIKZ
+
+import timeit
+
+# Código que queremos medir o desempenho
+def codigo_teste():
+  soma = 0
+  for i in range (900000):
+    soma += 1
+  return soma
+
+# Medindo o tempo de execução
+tempo_execucao = timeit.timeit(codigo_teste, number=1)
+print(f"Tempo de Execução: {tempo_execucao:.4f} segundos")
+
+Saida: Tempo de Execução: 0.0393 segundos
